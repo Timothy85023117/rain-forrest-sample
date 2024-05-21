ddae6d4174beea223f42a4f5a2238a3fd0d49b37
Timothy85023117 <s94kiwi9191@gmail.com>
2024-05-21 17:19:33 +0800

    修改stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -18,12 +18,12 @@ module.exports = {
   rules: {
     // "at-rule-no-unknown": null, 暫時先保留
     // 'selector-class-pattern': null, 暫時先保留
-    
+
     // 禁止在具有較高優先级的選擇器後出現被其覆蓋的較低優先级的選擇器
     'no-descending-specificity': null,
     
-    // class, id的命名可以使用a-z英文,--,__
-    "selector-class-pattern": "^[a-z][a-z-_]+$",
-    "selector-id-pattern": "^[a-z][a-z-_]+$"
+    // class, id的命名可以使用a-z英文和0-9數字,--,__
+    "selector-class-pattern": "^[a-z][a-z0-9-_]+$",
+    "selector-id-pattern": "^[a-z][a-z0-9-_]+$"
   },
 };
